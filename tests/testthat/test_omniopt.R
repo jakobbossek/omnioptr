3a264bb3aa42fcbde27f74358068780898856f7c
Jakob Bossek <info@jakobbossek.de>
2021-06-24 12:41:38 +0200

    add unit test

---
+++ tests/testthat/test_omniopt.R
@@ -0,0 +1,29 @@
+context("OmniOptimizer C Interface")
+
+test_that("omnioptr::omniopt produces correct outpur", {
+  # test functions
+  funs = c(
+    smoof::makeZDT1Function(dimension = 2L),
+    smoof::makeZDT2Function(dimension = 4L),
+    smoof::makeMMF6Function()
+  )
+  pop.size = 100L
+
+  for (f in funs) {
+    d = smoof::getNumberOfParameters(f)
+    o = smoof::getNumberOfObjectives(f)
+    res = omniopt(f, pop.size = pop.size, n.gens = 200L)
+    checkmate::expect_list(res)
+    checkmate::expect_matrix(res$pareto.set, nrows = d, ncols = pop.size, any.missing = FALSE, all.missing = FALSE)
+    checkmate::expect_matrix(res$pareto.front, nrows = o, ncols = pop.size, any.missing = FALSE, all.missing = FALSE)
+
+    # check whether fitness values of individuals match the smoof-output
+    expect_true(all(res$pareto.front == apply(res$pareto.set, 2L, f)))
+
+    # check if individuals respect box-constraints
+    lower = smoof::getLowerBoxConstraints(f)
+    upper = smoof::getUpperBoxConstraints(f)
+
+    expect_true(all(apply(res$pareto.set, 2L, function(e) { all((e >= lower) & (e <= upper))})))
+  }
+})
